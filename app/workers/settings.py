d7e7c0648def3886539ac941e3cf196a35c65701
siu0901 <trip0901@naver.com>
2026-09-02 16:50:38 +0900

    fix: worker settings 수정

--- app/workers/settings.py
+++ app/workers/settings.py
@@ -1,14 +1,17 @@
-from app.core.redis import get_redis
-from app.workers.email import (
-    send_verification_email,
-)
+from arq.connections import RedisSettings
 
+from app.core.config import get_settings
+
+from app.workers.email import send_verification_email
+
+
+settings = get_settings()
 
 class WorkerSettings:
     functions = [
         send_verification_email,
     ]
 
-    redis_settings = get_redis()
+    redis_settings = RedisSettings.from_dsn(settings.REDIS_URL)
 
     max_tries = 3
