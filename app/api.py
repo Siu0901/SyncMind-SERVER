22f3884f7e8020df7fa1656059faa8d98f0e1006
siu0901 <trip0901@naver.com>
2026-09-16 17:24:31 +0900

    fit: api 추가

--- app/api.py
+++ app/api.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.domains.auth.router import auth_router
 from app.domains.user.router import user_router
 from app.domains.workspace.router import workspace_router
+from app.domains.document.router import document_router
 
 
 router = APIRouter()
@@ -10,3 +11,4 @@ router = APIRouter()
 router.include_router(auth_router, tags=["auth"])
 router.include_router(user_router, tags=["users"])
 router.include_router(workspace_router, tags=["workspaces"])
+router.include_router(document_router, tags=["documents"])
